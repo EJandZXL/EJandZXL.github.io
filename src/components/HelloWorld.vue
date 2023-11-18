d6704d8552ba290350a48cef42936c68c8aab56d
ZhengEJ <786961252@qq.com>
2023-11-18 20:05:12 +0800

    add problem and aplayer

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,23 @@
   <div>
     <h1 class="shine" align="center">{{ msg }}</h1>
     <input v-model = "ans" align="center">
-    <p>
+    <p class="tips">tips:输入正确答案，答案在书中哦</p>
     <img src="../assets/DM_20231106203830_001.png" v-on:click="check_the_ans">
-    </p>
-    <router-link to="/rightanswer">ans</router-link>
+    <table class="table1" align="center">
+      <tbody>
+        <tr>
+          <th>神秘代码</th>
+        </tr>
+        <tr><td>0400626</td></tr>
+        <tr><td>0572120</td></tr>
+        <tr><td>0640707</td></tr>
+        <tr><td>0731604</td></tr>
+        <tr><td>0770101</td></tr>
+        <tr><td>0850610</td></tr>
+        <tr><td>1080103</td></tr>
+        <tr><td>1080104</td></tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -16,6 +29,15 @@ export default {
     return {
       msg: '给网安人的小情书',
       ans: ''
+    }
+  },
+  methods: {
+    check_the_ans () {
+      if (this.ans === '爱你直至宇宙尽头') {
+        this.$router.push('/rightanswer')
+      } else {
+        this.ans = '再试试吧，你可以的'
+      }
     }
   }
 }
@@ -63,6 +85,25 @@ input {
   100% {
      box-shadow: 0 0 0 20px rgba(255,255,255,0);
   }
+}
+.tips {
+  text-align: center;
+  color: white;
+  border-color: white;
+}
+.table1 {
+  box-align: center;
+  text-align: center;
+  color: white;
+
+}
+th {
+  font-size: 20px;
+}
+td {
+  border-bottom-style: solid;
+  border-color: white;
+  font-family: 幼圆;
 }
 ul {
   list-style-type: none;
--- src/components/rightanswer.vue
+++ src/components/rightanswer.vue
@@ -1,9 +1,47 @@
 <template>
-    <h1>congritulations!</h1>
+    <div id="container">
+    <div id="header">
+      <h1 class="h_c">四周年快乐!</h1>
+    </div>
+    <div id="content">
+
+    </div>
+    <div id="footer">
+      <aplayer :music="audio[0]" :list="audio" :mini="true" :autoplay="true" :repeat="repeat-all"></aplayer>
+    </div>
+    </div>
 </template>
 
 <script>
+import aplayer from 'vue-aplayer'
 export default {
+  components: {
+    aplayer
+  },
+  data () {
+    return {
+      audio: [
+        {
+          title: '参宿四',
+          artist: 'dk',
+          url: '../src/assets/DK - ベテルギウス.flac',
+          pic: '../src/assets/参宿四.jpg'
+        },
+        {
+          title: '开普勒',
+          artist: '孙燕姿',
+          url: '../src/assets/孙燕姿 - 克卜勒.flac',
+          pic: '../src/assets/参宿四.jpg'
+        }
+      ]
+    }
+  },
   name: 'rightanswer'
 }
 </script>
+<style>
+.h_c {
+  color: white;
+  font-size: large;
+}
+</style>
